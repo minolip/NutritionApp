977bf48ba3c9b02f8a16390049fed5b22a67b147
minoli <minoli@liveroom.xyz>
2021-07-03 23:38:45 +0530

    load custom meal plan through child component

--- src/components/CutomMealPlan.js
+++ src/components/CutomMealPlan.js
@@ -1,21 +1,125 @@
-import react from 'react';
-import './CustomMealPlan.css';
+import React, { useState } from 'react';
+import {
+    TextField,
+    Button
+} from '@material-ui/core/';
+import { makeStyles } from '@material-ui/core/styles';
+import { MealTime } from '@suggestic/sdk/dist/__generated_sdk';
+import { Suggestic } from "@suggestic/sdk";
+require('dotenv').config();
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: '#DAF7A6',
+        alignItems: 'center'
+    },
+
+    requestForm: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+    },
+}));
 
 const CustomMealPlan = () => {
-    const requestCustomMealPlan = () => {
-        console.log('click submit button');
+
+    const classes = useStyles();
+    const client = new Suggestic(process.env.REACT_APP_TOKEN);
+    const user = client.getUser(process.env.REACT_APP_USER_ID);
+
+    const [caloryLevel, setCaloryLevel] = useState(0);
+    const [carbsLevel, setCarbsLevel] = useState(0);
+    const [proteinLevel, setProteinLevel] = useState(0);
+    const [fatsLevel, setFatsLevel] = useState(0);
+
+    const changeCaloryLevel = (e) => {
+        setCaloryLevel(e.target.value);
     }
 
-    return(
-        <div className="container">
+    const changeCarbsLevel = e => {
+        setCarbsLevel(e.target.value);
+    }
+
+    const changeProteinLevel = e => {
+        setProteinLevel(e.target.value);
+    }
+
+    const changeFatLevel = e => {
+        setFatsLevel(e.target.value);
+    }
+
+    const getCustomMealPlan = async (props) => {
+        const customMealplan = await user.customMealPlan(props);
+        console.log(customMealplan);
+    }
+
+    const requestCustomMealPlan = () => {
+        const data = {
+            calories: parseFloat(caloryLevel),
+            carbs: parseFloat(carbsLevel),
+            protein: parseFloat(proteinLevel),
+            fat: parseFloat(fatsLevel),
+            days: 1,
+            format: [MealTime.Breakfast, MealTime.Lunch, MealTime.Dinner]
+        };
+        getCustomMealPlan(data);
+    }
+
+    return (
+        <div className={classes.container}>
             <h2>Request Custom Meal Plan</h2>
-            <form className="requestForm" onSubmit={requestCustomMealPlan}>
-                <input type="text" name="calory" placeholder="Maximum number of calories do you expect"></input>
-                <input type="text" name="carbs" placeholder="Maximum number of carbohidrate level do you expect"></input>
-                <input type="text" name="protein" placeholder="Maximum number of protein level do you expect"></input>
-                <input type="text" name="fats" placeholder="Maximum number of fat level do you expect"></input>
-                <button type="submit">Submit</button>
-            </form>
+            <div className={classes.requestForm}>
+                <div className={classes.root}>
+                    <TextField
+                        required
+                        id="calory"
+                        label="Calories"
+                        variant="filled"
+                        value={caloryLevel}
+                        onChange={changeCaloryLevel}
+                    />
+                    <TextField
+                        required
+                        id="carbs"
+                        label="Carbohidrates"
+                        variant="filled"
+                        value={carbsLevel}
+                        onChange={changeCarbsLevel}
+                    />
+                    <TextField
+                        required
+                        id="protein"
+                        label="Protein"
+                        variant="filled"
+                        value={proteinLevel}
+                        onChange={changeProteinLevel}
+                    />
+                    <TextField
+                        required
+                        id="fats"
+                        label="Fats"
+                        variant="filled"
+                        value={fatsLevel}
+                        onChange={changeFatLevel}
+                    />
+                </div>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={requestCustomMealPlan}
+                >
+                    Submit
+                </Button>
+            </div>
         </div>
     );
 }
